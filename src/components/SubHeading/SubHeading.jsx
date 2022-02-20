0626e2cac8c2a7760267f76fba47eaef411556ba
tinku gupta <tinku.gupta4@gmail.com>
2022-02-20 10:39:26 +0530

    adding header section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({tittle}) => (
+  <div style={{marginBottom:'1rem'}}>
+    <p className='p__cormorant'>{tittle}</p>
+    <img src={images.spoon}  alt="spoon" className='spoon__img' />
   </div>
 );
 
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './AboutUs.css';
+import {images} from '../../constants';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__paddinh' id='about'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="" />
+    </div>
   </div>
 );
 
